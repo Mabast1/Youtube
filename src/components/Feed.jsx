cb11968c658e9752bd7117f291d2e456c0f71365
Mabast <mabast.niga@yahoo.com>
2022-08-30 21:51:02 -0500

    created Feeds component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
 
 const Feed = () => {
-  return <div>Feed</div>;
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sm: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sm: 0, md: 2 },
+        }}
+      >
+        Sidebar
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "gray" }}
+        >
+          Copyright 2022{" "}
+          <a
+            href="https://mabastahmad.com"
+            target="_blank"
+            rel="noreferrer"
+            style={{ color: "white", textDecoration: "underline" }}
+          >
+            Mabast Ahmad
+          </a>
+        </Typography>
+      </Box>
+    </Stack>
+  );
 };
 
 export default Feed;
