b0c5191d08452b4eff1c95c39aac93fd29ebab0e
Mabast <mabast.niga@yahoo.com>
2022-09-04 22:47:21 -0500

    added new channel

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -11,11 +11,13 @@ import GraphicEqIcon from "@mui/icons-material/GraphicEq";
 import TheaterComedyIcon from "@mui/icons-material/TheaterComedy";
 import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
 import DeveloperModeIcon from "@mui/icons-material/DeveloperMode";
+import LaptopChromebookIcon from "@mui/icons-material/LaptopChromebook";
 
 export const logo = "https://i.ibb.co/s9Qys2j/logo.png";
 
 export const categories = [
   { name: "New", icon: <HomeIcon /> },
+  { name: "MKBHD", icon: <LaptopChromebookIcon /> },
   { name: "Coding", icon: <CodeIcon /> },
   { name: "ReactJS", icon: <CodeIcon /> },
   { name: "Web 3.0", icon: <CodeIcon /> },
